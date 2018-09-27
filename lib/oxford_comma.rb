f2aa3bc39130727d58a35a25eabd472adaf85af1
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-27 19:09:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+if array.length ==1 
+  return "#{array[0]}" 
+elsif array.length ==2 
+return array.join(" and ")
+elsif array.length>=3
+oxfordarray = []
+oxfordarray << array[-1]
+array.pop
+oxfordarray.insert(0, "and")
+array << oxfordarray.join(" ")
+return array.join(", ")
+end 
 end
