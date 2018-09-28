f70cea6a79832e23b21f7805a0171b21c50bce14
hmalik88 <hbmalik88@gmail.com>
2018-09-27 22:20:43 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+	if array.size == 1
+		return array[0]
+	elsif array.size == 2
+		return array.join(" and ")
+	elsif array.size == 3
+		first_two = [array[0], array[1]]
+		first_two = first_two.join(", ")
+		array = [first_two, array[2]]
+		return array.join(", and ")
+	elsif array.size > 3
+		all_but_last = []
+		for count in 0..(array.size - 2) do
+			all_but_last.push(array[count])
+		end
+		all_but_last = all_but_last.join(", ")
+		array = [all_but_last, array[-1]]
+		return array.join(", and ")
+	end
 end
